06337105c89a1e929d29aeb4eca12253127c6f40
Gabino Hernandez <gahergod@hotmail.com>
2023-08-05 11:52:11 -0600

    Reestructuracion para poder hacer que todo el codigo fuera ajustable a la pantalla

--- word_counter.py
+++ word_counter.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import scrolledtext
 
 def count_words():
-    text = text_area.get("1.0", tk.END)
+    text = text_area.get("1.0", 'end-1c')
     words = text.split()
     word_count = len(words)
     count_label.config(text=f"Word count: {word_count}")
@@ -10,28 +10,51 @@ def count_words():
 root = tk.Tk()
 root.title("Word Counter")
 
-#exit button and functionality
-exit_button = tk.Button(root, text='X', command=root.quit)
-exit_button.pack(side=tk.RIGHT, anchor=tk.N)
+# Get screen size
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-#creacion de un frame para poder tener todo al centro
+# Calculate width and height for root window
+root_width = int(screen_width * 0.7)
+root_height = int(screen_height * 0.7)
+
+# Set root window size and position it at center
+x_position = int((screen_width / 2) - (root_width / 2))
+y_position = int((screen_height / 2) - (root_height / 2))
+
+root.geometry(f'{root_width}x{root_height}+{x_position}+{y_position}')
+
+# Create a frame
 frame = tk.Frame(root)
-frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+frame.pack(expand=True, fill=tk.BOTH)
 
-#title label
-title_label = tk.Label(frame, text="Word Counter", font=("Helvetica", 24))
-title_label.pack()
+# Title and exit frame
+title_exit_frame = tk.Frame(frame)
+title_exit_frame.grid(row=0, column=0, sticky='ew')
 
-text_area = scrolledtext.ScrolledText(frame, width=30, height=10, font=("Times New Roman", 18))
-text_area.pack()
+# Title label
+title_label = tk.Label(title_exit_frame, text="Word Counter", font=("Helvetica", 24))
+title_label.pack(side=tk.LEFT, expand=True)
 
+# Exit button and functionality
+exit_button = tk.Button(title_exit_frame, text='X', command=root.quit)
+exit_button.pack(side=tk.RIGHT)
+
+# Text area
+text_area = scrolledtext.ScrolledText(frame, font=("Times New Roman", 18))
+text_area.grid(row=1, column=0, sticky='nsew')
+
+# Count words button
 count_button = tk.Button(frame, text="Count Words", command=count_words)
-count_button.pack()
+count_button.grid(row=2, column=0, sticky='ew')
 
+# Words counted label
+count_label = tk.Label(frame, text="", font=("Helvetica", 14), padx=10, pady=10)
+count_label.grid(row=3, column=0, sticky='ew')
 
-#words counted text
-count_label = tk.Label(frame,text="")
-count_label.pack()
+# Configure the rows and columns of the grid to expand.
+frame.grid_rowconfigure(1, weight=1)  # Make the text_area row expandable.
+frame.grid_columnconfigure(0, weight=1)  # Make the column expandable.
 
 root.mainloop()
 
