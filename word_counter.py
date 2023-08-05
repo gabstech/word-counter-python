bf557b08f6f3cc4140ee0c5577e8a3b394d33e39
Gabino Hernandez <gahergod@hotmail.com>
2023-08-05 11:25:04 -0600

    se agrego y centro un frame

--- word_counter.py
+++ word_counter.py
@@ -14,19 +14,23 @@ root.title("Word Counter")
 exit_button = tk.Button(root, text='X', command=root.quit)
 exit_button.pack(side=tk.RIGHT, anchor=tk.N)
 
+#creacion de un frame para poder tener todo al centro
+frame = tk.Frame(root)
+frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
 #title label
-title_label = tk.Label(root, text="Word Counter", font=("Helvetica", 24))
+title_label = tk.Label(frame, text="Word Counter", font=("Helvetica", 24))
 title_label.pack()
 
-text_area = scrolledtext.ScrolledText(root, width=30, height=10, font=("Times New Roman", 18))
+text_area = scrolledtext.ScrolledText(frame, width=30, height=10, font=("Times New Roman", 18))
 text_area.pack()
 
-count_button = tk.Button(root, text="Count Words", command=count_words)
+count_button = tk.Button(frame, text="Count Words", command=count_words)
 count_button.pack()
 
 
 #words counted text
-count_label = tk.Label(root,text="")
+count_label = tk.Label(frame,text="")
 count_label.pack()
 
 root.mainloop()
