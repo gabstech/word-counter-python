221d1e8493df99d98259de56708e86257a0655d4
Gabino Hernandez <gahergod@hotmail.com>
2023-08-05 11:03:55 -0600

    Primera prueba con funcionalidades basicas de un alert

---
+++ word_counter.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import messagebox, scrolledtext
+
+def count_words():
+    text = text_area.get("1.0", tk.END)
+    words = text.split()
+    word_count = len(words)
+    messagebox.showinfo("Word count", f"Word count: {word_count}")
+
+root = tk.Tk()
+root.title("Word Counter")
+
+text_area = scrolledtext.ScrolledText(root, width=30, height=10, font=("Times New Roman", 20))
+text_area.pack()
+
+count_button = tk.Button(root, text="Count Words", command=count_words)
+count_button.pack()
+
+root.mainloop()
+
